63b38c75d716b177b7198c00cb92eecbd83ea925
tomi <tomislav.varga@icloud.com>
2025-02-20 10:16:44 +0100

    Initial commit

---
+++ test/task-3-backend-rs-school-aws-course.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Task3BackendRsSchoolAwsCourse = require('../lib/task-3-backend-rs-school-aws-course-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/task-3-backend-rs-school-aws-course-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Task3BackendRsSchoolAwsCourse.Task3BackendRsSchoolAwsCourseStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
