606b576854db11960d72431775fab2bdd723d8db
tomi <tomislav.varga@icloud.com>
2025-02-21 17:25:48 +0100

    solution task 3.1

--- bin/task-3-backend-rs-school-aws-course.js
+++ bin/task-3-backend-rs-school-aws-course.js
@@ -1,21 +1,69 @@
 #!/usr/bin/env node
-
 const cdk = require('aws-cdk-lib');
-const { Task3BackendRsSchoolAwsCourseStack } = require('../lib/task-3-backend-rs-school-aws-course-stack');
+const lambda = require('aws-cdk-lib/aws-lambda');
+const apigateway = require('aws-cdk-lib/aws-apigateway');
+
+const products = [
+  {
+    id: '1',
+    title: 'Gaming Laptop',
+    description: 'High-performance gaming laptop with RTX 4080',
+    price: 1999.99,
+    count: 10
+  },
+  {
+    id: '2',
+    title: 'Wireless Headphones',
+    description: 'Noise-cancelling Bluetooth headphones',
+    price: 249.99,
+    count: 15
+  },
+  {
+    id: '3',
+    title: 'Mechanical Keyboard',
+    description: 'RGB mechanical keyboard with Cherry MX switches',
+    price: 159.99,
+    count: 20
+  },
+  {
+    id: '4',
+    title: 'Ultra-wide Monitor',
+    description: '34-inch curved gaming monitor, 144Hz',
+    price: 499.99,
+    count: 5
+  }
+];
+
+class Task3BackendRsSchoolAwsCourseStack extends cdk.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Lambda function
+    const getProductsFunction = new lambda.Function(this, 'GetProductsFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.getProductsList',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        PRODUCTS: JSON.stringify(products) // Pass products as environment variable
+      }
+    });
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'ProductsApi', {
+      restApiName: 'Products Service'
+    });
+
+    const productsResource = api.root.addResource('products');
+    productsResource.addMethod('GET', new apigateway.LambdaIntegration(getProductsFunction));
+  }
+}
 
 const app = new cdk.App();
 new Task3BackendRsSchoolAwsCourseStack(app, 'Task3BackendRsSchoolAwsCourseStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
 });
+
+module.exports = { Task3BackendRsSchoolAwsCourseStack }
---
+++ lambda/index.js
@@ -0,0 +1,26 @@
+exports.getProductsList = async (event) => {
+    try {
+      const products = JSON.parse(process.env.PRODUCTS);      
+  
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify(products)
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({ message: 'Internal server error' })
+      };
+    }
+  };
+  
